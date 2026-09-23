eb1c2d1de2059045612cf5003d3ed905dd4cdfc3
shivamsonicmaug26ac <shivam.soni.cmaug26@gmail.com>
2026-09-23 11:35:14 +0530

    Add files via upload

---
+++ Short_assignment 21.9.2026/Ques.java
@@ -0,0 +1,100 @@
+package soni.org;
+import java.util.*;
+
+import java.time.LocalDate;
+//import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Supplier;
+import java.util.Scanner;
+
+public class Ques {
+
+	public static void main(String[] args) {
+//		que1
+//		List<String> s =  Arrays.asList("Shivam", "Arun", "Seema", "Bhumi");
+//		Collections.sort(s);
+//		System.out.println(s);
+
+//		que2
+//		List<Integer> s =  Arrays.asList(55,88,99,66);		
+//		int largest = Collections.max(s);
+//		System.out.println(largest);
+
+//		que3
+//		List<Integer> s =  Arrays.asList(55,88,99,66);		
+//		int smallest = Collections.min(s);
+//		System.out.println(smallest);
+		
+//		que4
+//		Supplier<Integer> s = () -> (int)(Math.random()*1000);
+//		System.out.println(s.get());
+		
+//		que5
+//		int[] arr  = {10,20,30,40,50};
+//		
+//		Function<int[], int[]> reverse = (a) ->{
+//			
+//			int[] rev = new int[a.length];
+//			
+//			for(int i=0; i<a.length; i++) {
+//				rev[i] = a[a.length - 1 - i];
+//			}
+//			return rev;
+//		};
+//		int[] result = reverse.apply(arr);
+//		
+//		System.out.println(Arrays.toString(result));
+//	}
+//}
+		
+//		que6
+//		Supplier<LocalDate> date = () -> LocalDate.now();
+//			System.out.println(date.get());
+		
+//		que7
+
+//		Scanner sc = new Scanner(System.in);
+//
+//		System.out.print("Enter number: ");
+//		int num = sc.nextInt();
+//
+//		Predicate<Integer> prime = (n) -> {
+//			if (n < 2)
+//				return false;
+//
+//			for (int i = 2; i < n; i++) {
+//				if (n % i == 0)
+//					return false;
+//			}
+//
+//			return true;
+//		};
+//
+//		System.out.println(prime.test(num));
+		
+//		que8
+//		Scanner sc = new Scanner(System.in);
+//
+//        System.out.print("Enter first string: ");
+//        String s1 = sc.nextLine();
+//
+//        System.out.print("Enter second string: ");
+//        String s2 = sc.nextLine();
+//
+//        BiFunction<String, String, String> concat = (a, b) -> a + b;
+//
+//        String result = concat.apply(s1, s2);
+//
+//        System.out.println("Concatenated String: " + result);
+//		
+		
+			
+		}
+		}		
